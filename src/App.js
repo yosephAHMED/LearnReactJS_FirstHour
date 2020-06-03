f0e5a3ad695553dc36abd1902e39f5e46fa4692f
YosephAhmed <yosephahmed@aol.com>
2020-06-02 21:22:17 -0400

    Completed Task 1 of React JS tutorial.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
 import React from 'react';
 
+// Objective: Fill in the boilerplate React code required to render an
+// unordered list (<ul>) to the page. The list should contain 3 list items
+// (<li>) with anything in them you want.
+
 function App() {
   return (
-    <h1>Hello world!</h1>
+    // To fix "JSX Expressions must have one parent element error,"
+    // I can enclose all tags in a div, or comment out the <h1> tag
+
+    <div>
+      <h1>Hello world!</h1>
+      <ul>
+        <li>Peaches</li>
+        <li>Apples</li>
+        <li>Bananas</li>
+      </ul>
+    </div>
+    
   );
 }
 

81faec7c97c3905bbb57e843af8a9779c87b416a
YosephAhmed <yosephahmed@aol.com>
2020-06-02 22:20:29 -0400

    Fixed formatting for App.js. Created and finished task 2 in file 'MyInfo.js'. Added MyInfo module to index.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
-
-// Objective: Fill in the boilerplate React code required to render an
+// Objective 1
+// Fill in the boilerplate React code required to render an
 // unordered list (<ul>) to the page. The list should contain 3 list items
 // (<li>) with anything in them you want.
 
 function App() {
   return (
+
     // To fix "JSX Expressions must have one parent element error,"
     // I can enclose all tags in a div, or comment out the <h1> tag
 
@@ -17,7 +18,6 @@ function App() {
         <li>Bananas</li>
       </ul>
     </div>
-    
   );
 }
 
